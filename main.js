3be14ff7b117a7f5e703da8945110b25e37218da
Anna <anna.silverman@concordacademy.org>
2019-04-08 13:21:25 -0400

    mouse/player move

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = innerWidth-20;
+canvas.height = innerHeight-20;
+
+let mousepos;
 
 let colors = [
   "red",
@@ -19,15 +21,20 @@ function randomColor() {
   return colors[index];
 }
 
-
+let player;
 let foods = [];
 
 function init() {
+
+  mousepos = new Vector(canvas.width/2, canvas.height/2);
+
+  player = new Player(canvas.width/2, canvas.height/2, 30, randomColor());
   for (var i = 0; i <= 100; i++) {
     let x = Math.random() * canvas.width;
     let y = Math.random() * canvas.height;
+    let radius = 10;
     let color = randomColor();
-    let food = new Food(x, y, 10, color);
+    let food = new Food(x, y, radius, color);
     foods.push(food);
   }
   update();
@@ -35,14 +42,26 @@ function init() {
 
 function update() {
   c.clearRect(0,0,canvas.width, canvas.height);
+  player.x = mousepos.x;
+  player.y = mousepos.y;
   for(var i = 0; i < foods.length; i++){
     foods[i].draw(c);
   }
+
+  player.draw(c);
+
   requestAnimationFrame(update);
 }
 
 window.addEventListener('load', function(event) {
   init();
 
+  window.addEventListener('mousemove', function(event){
+      //console.log(event.clientX, event.clientY);
+      mousepos.x = event.clientX - canvas.offsetLeft;
+      mousepos.y = event.clientY - canvas.offsetTop;
+      mousepos.print();
+
+  });
 
 });
